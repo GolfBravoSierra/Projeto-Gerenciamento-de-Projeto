4de95888bb821ba01648b13752849cba9c9fa3dd
VRoston <victor.mr1@puccampinas.edu.br>
2024-10-17 14:47:43 -0300

    Login e LoginTest
    17/10/2024 14:47

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
 
 
-Route::get('/register', function () {
-    return view('register');
-
-});
+Route::get('/register',[UserController::class, 'create']);
 Route::post('/register',[UserController::class, 'store']);
+
+Route::get('/login',[LoginController::class, 'create']);
+Route::post('/login',[LoginController::class, 'store']);
+Route::post('/logout',[LoginController::class, 'destroy']);
