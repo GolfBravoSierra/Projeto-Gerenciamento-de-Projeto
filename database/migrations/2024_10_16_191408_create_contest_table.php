a23b3ec8e8f5e0a101b381dfa6a86da98374acf1
VRoston <victor.mr1@puccampinas.edu.br>
2024-10-17 21:32:40 -0300

    Index
    17/10/2024 21:32

--- database/migrations/2024_10_16_191408_create_contest_table.php
+++ database/migrations/2024_10_16_191408_create_contest_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contest', function (Blueprint $table) {
+        Schema::create('contests', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('title');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contest');
+        Schema::dropIfExists('contests');
     }
 };
