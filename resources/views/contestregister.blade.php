fca966284bb4cc84378435bd493435e26eb68fef
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2024-10-17 21:46:22 -0300

    Refactor registration form layout and styles

--- resources/views/contestregister.blade.php
+++ resources/views/contestregister.blade.php
@@ -3,23 +3,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Usuário</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <h2>Criação de Campeonatos</h2>
-        <form action="/action_page.php">
-            <label for="title">Nome do Campeonato:</label>
-                <input type="text" id="title" name="title"><br><br>
-                    <label for="mode">Modo:</label>
-                        <input type="text" id="mode" name="mode"><br><br>
-                            <label for="begin_date">Data de Inicio:</label>
-                            <input type="datetime-local" id="begin_date" name="begin_date"   value="2018-06-12T19:30" min="2018-06-07T00:00" max="2018-06-14T00:00"  />
-                            <label for="end_date">Data de Termino:</label>
-                            <input type="datetime-local" id="end_date" name="end_date" value="2018-07-22" min="2018-01-01" max="2018-12-31" /><br><br>
-                    <label for="description">Descrição do Campeonato:</label><br>
-                <textarea id="description" name="description" rows="4" cols="50"></textarea><br><br>
-            <input type="submit" value="Submit">
-        </form>
+        <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+            <div class="w-50">
+                <h2 class="text-center">Criação de Campeonatos</h2>
+                <form action="/action_page.php">
+                    <div class="form-group">
+                        <div class="form-floating">
+                            <label for="title">Nome do Campeonato</label>
+                            <input type="text" id="title" class="form-control" name="title">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-floating">
+                            <label for="mode">Modo</label>
+                            <input type="text" id="mode" class="form-control" name="mode">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-floating">
+                            <label for="begin_date">Data de Inicio</label>
+                            <input class="form-control" type="datetime-local" id="begin_date" name="begin_date"   value="2018-06-12T19:30" min="2018-06-07T00:00" max="2018-06-14T00:00"  />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-floating">
+                            <label for="end_date">Data de Termino</label>
+                            <input class="form-control" type="datetime-local" id="end_date" name="end_date" value="2018-07-22" min="2018-01-01" max="2018-12-31" />
+                        </div>
+                    </div>  
+                    <div class="form-group">
+                        <div class="form-floating">
+                            <label for="description">Descrição do Campeonato</label>
+                            <textarea class="form-control" id="description" name="description" rows="4" cols="50"></textarea>
+                        </div>
+                    </div> 
+                    <input type="submit" class="btn btn-primary btn-block" value="Submit">
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>
