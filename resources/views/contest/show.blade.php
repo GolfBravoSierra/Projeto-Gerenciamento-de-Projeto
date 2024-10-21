4e36835e45e9f17c23538cc79cf86f4beaac9d96
VRoston <victor.mr1@puccampinas.edu.br>
2024-10-21 20:36:56 -0300

    Update

--- resources/views/contest/show.blade.php
+++ resources/views/contest/show.blade.php
@@ -3,6 +3,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Show Contest</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"></script>
 </head>
 <body>
     <div>
@@ -21,11 +24,21 @@
         </div>
         <br>
         <div>
-            <h4>
-                Participantes registrados:
-            </h4>
-            <table>
-                
+            <table class="table">
+                <caption>Participantes registrados:</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Usuário</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($contest->users as $user)
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->user_name}}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     </div>
