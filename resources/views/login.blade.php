7761d0f56833c10294adab885d6d2f8032443f3a
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2024-10-21 21:25:55 -0300

    Refactor login and register forms

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,28 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login de Usuário</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <h2>Login de Usuário</h2>
-        <form action="/login" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="email">E-mail:</label>
-                <input type="email" class="form-control" id="email" name="email" required>
-                @error('email')
-                    <small class="error">**{{ $message }}**</small>
-                @enderror
-            </div>
-            <div class="form-group">
-                <label for="password">Senha:</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-                @error('password')
-                    <small class="error">**{{ $message }}**</small>
-                @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Enviar</button>
-        </form>
+    <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+        <div class="w-50">
+            <h2 class="text-center">Login de Usuário</h2>
+            <form action="/login" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="email">E-mail:</label>
+                    <input type="email" class="form-control" id="email" name="email" required>
+                    @error('email')
+                        <small class="error">**{{ $message }}**</small>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" class="form-control" id="password" name="password" required>
+                    @error('password')
+                        <small class="error">**{{ $message }}**</small>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Enviar</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
